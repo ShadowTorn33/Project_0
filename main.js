298471d21538ee8931d950889845372a47e909a7
ShadowTorn33 <austincheer94@gmail.com>
2022-11-10 16:44:49 -0800

    Added new variables linked to HTML

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const restBtn=document.querySelector('.sleepiness')
 const feedPetBtn=document.querySelector('.hunger')
 const playBtn=document.querySelector('.boredom')
 const age=document.querySelector('#age')
+const hungerLevel=document.querySelector('#hunger')
+const boredomLevel=document.querySelector('#boredom')
+const sleepinessLevel=document.querySelector('#sleepiness')
 
 const myPet = {
     hunger: 0,
